16a7e885cf7aca47ef81603d61c50cdef41b98c4
Leotju <xj65112338>
2019-09-25 21:11:04 +0800

    0925

--- train_ca.py
+++ train_ca.py
@@ -94,7 +94,7 @@ class Dataset():
     def __init__(self):
         # ImageRoot = './data/coco2017/train2017'
         # AnnoFile = './data/coco2017/annotations/person_keypoints_train2017_pose2seg.json'
-        ImageRoot = '../data/coco2017/images/train2017'
+        ImageRoot = '../data/coco2017/train2017'
         AnnoFile = '../data/coco2017/annotations/person_keypoints_train2017_pose2seg.json'
 
         self.datainfos = CocoDatasetInfo(ImageRoot, AnnoFile, onlyperson=True, loadimg=True)
